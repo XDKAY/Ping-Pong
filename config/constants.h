ee634e7329e2d5506fb7a04a57fc27a785a534fe
XDKAY <sasha.naftaliev.02@mail.ru>
2026-07-24 23:42:19 +0300

    feat: add constants

---
+++ config/constants.h
@@ -0,0 +1,23 @@
+// Field
+#define ROWS 25
+#define COLS 80
+
+// Rackets
+#define RACKET_LEN 3
+#define RACKET_MARGIN 2
+
+#define RACKET_LEFT_POS_X (RACKET_MARGIN)
+#define RACKET_LEFT_START_Y (ROWS / 2 - 1)
+
+#define RACKET_RIGHT_POS_X (COLS - RACKET_MARGIN - 1)
+#define RACKET_RIGHT_START_Y (ROWS / 2 - 1)
+
+// Ball
+#define BALL_START_X (COLS / 2)
+#define BALL_START_Y (ROWS / 2)
+#define SPEED_BALL 80
+
+// Scores
+#define SCORE_FIRST_PLAYER 0
+#define SCORE_SECOND_PLAYER 0
+#define WIN_SCORE 21
